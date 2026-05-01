c729b3f49700ec1cb6d49525e74ede1b8b00a276
Alex-ifDev <editdevops@gmail.com>
2026-05-01 19:31:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 from aiogram.types import FSInputFile
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8716094991:AAEHEjbLhnl4z9rD0MTETd2yGjYPr8R7AqQ"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
